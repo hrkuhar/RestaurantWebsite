d81fb6fcdba9a125097601252912876183a6c219
Hrvoje Kuhar <hrkuhar@vsite.hr>
2019-01-28 18:42:13 +0100

    login page adjustments

---
+++ RestaurantWebsite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantWebsite.Data;
+
+[assembly: HostingStartup(typeof(RestaurantWebsite.Areas.Identity.IdentityHostingStartup))]
+namespace RestaurantWebsite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
